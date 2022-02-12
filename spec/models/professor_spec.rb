49edf5e1a76b53fd38d3dc71a5db4fd6b6b59dd4
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-12 14:04:07 -0700

    Add model method to model testing

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  it '#average_student_age' do
+    prof1 = Professor.create!(name: "Snape", age: 55, specialty: "Dark")
+    stud1 = prof1.students.create!(name: "Neville", age: 15, house: "Hufflepuff")
+    stud2 = prof1.students.create!(name: "Hermione", age: 15, house: "Ravenclaw")
+    stud3 = prof1.students.create!(name: "Harry", age: 10, house: "Hugglepuff")
+
+    expect(prof1.average_student_age).to eq(13.3)
+  end
 end
