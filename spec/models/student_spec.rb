404663163f6a7f63a49aedd697a59c34ac48e5cf
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-12 14:10:02 -0700

    Add prof count method to model and add model testing for student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  it '#prof_count' do
+    @prof1 = Professor.create!(name: "Professor", age: 50, specialty: "magic")
+    @prof2 = Professor.create!(name: "Professor 2", age: 56, specialty: "magic again")
+
+    @stud1 = @prof1.students.create!(name: "Student 1", age: 12, house: "Hufflepuff")
+    @stud_prof = ProfessorStudent.create!(professor: @prof2, student: @stud1)
+
+    expect(@stud1.prof_count).to eq(2)
+  end
 end
